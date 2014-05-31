2159156a6c01f000f6442212605bb23c52c1f62c
Cas Rusnov <rusnovn@gmail.com>
2014-05-31 00:42:28 -0700

    start.sh tweak

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-gunicorn -e "PIXYWERK_CONFIG=/home/production/conf/pixyfoo.conf" --pythonpath "/home/production/pixywerk/" --pythonpath "/home/production/ppcode" pixywerk.wsgi:do_werk
+export PIXYWERK_CONFIG=/home/production/conf/pixyfoo.conf
+gunicorn --pythonpath "/home/production/pixywerk/" --pythonpath "/home/production/ppcode" pixywerk.wsgi:do_werk
